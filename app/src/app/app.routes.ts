65bf51a514c0a1a4882933d0f0573dd4c77390c8
Ali Murtaza Rizvi <murtazarizvi407@gmail.com>
2025-04-19 18:11:38 +0500

    Login/Signup Added

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './Pages/home/home.component';
 import { CartComponent } from './Pages/cart/cart.component';
 import { ProductPageComponent } from './Pages/product-page/product-page.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { SignupComponent } from './Pages/auth/signup/signup.component';
 
 export const routes: Routes = [
     {
@@ -16,6 +18,13 @@ export const routes: Routes = [
         path: 'product/:productName',
         component: ProductPageComponent 
     },
-    
+    {
+        path:'auth/login',
+        component:LoginComponent
+    },
+    {
+        path:'auth/signup',
+        component:SignupComponent
+    }
     
 ];
