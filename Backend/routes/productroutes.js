248886c19a105448ade32f313f21002d6401ca7e
Ali Murtaza Rizvi <murtazarizvi407@gmail.com>
2025-04-29 20:22:17 +0500

    product route fixed

--- Backend/routes/productroutes.js
+++ Backend/routes/productroutes.js
@@ -6,14 +6,16 @@ const {
     getProductById,
     updateProduct,
     deleteProduct,
-} = require("../controllers/productController"); // Adjust path as necessary
+} = require("../controllers/productcontroller"); // Adjust path as necessary
 const isAdmin = require("../middlewares/isAdmin"); // Middleware for admin protection
 
 // Public routes
 router.get("/", getAllProducts);
 router.get("/:id", getProductById);
 
+///api/products
 // Admin-only routes
+// router.post("/add", addProduct);
 router.post("/", isAdmin, addProduct);
 router.put("/:id", isAdmin, updateProduct);
 router.delete("/:id", isAdmin, deleteProduct);
