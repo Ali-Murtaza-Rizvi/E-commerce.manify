103cb137fa73ca979e57597fd71cc6ff86f312c5
MujtabaRizvi01 <mujtabarizvi18@gmail.com>
2025-05-12 01:03:58 +0500

    issues resolved

--- app/src/app/website-layout/Pages/product-page/you-may-like/you-may-like.component.ts
+++ app/src/app/website-layout/Pages/product-page/you-may-like/you-may-like.component.ts
@@ -1,8 +1,4 @@
-<<<<<<< Updated upstream
-import { Component, OnInit } from '@angular/core';
-=======
 import { Component,Input,OnInit } from '@angular/core';
->>>>>>> Stashed changes
 import { CartService } from '../../../services/cart.service';
 import { CommonModule, CurrencyPipe } from '@angular/common';
 import { ProductsService } from '../../../../admin-layout/adminservices/products.service';
@@ -16,41 +12,20 @@ import { Router } from '@angular/router';
   templateUrl: './you-may-like.component.html',
   styleUrls: ['./you-may-like.component.scss']
 })
-<<<<<<< Updated upstream
-export class YouMayLikeComponent implements OnInit {
-  YouMayLikeProducts: any[] = [];
-=======
 export class YouMayLikeComponent {
   @Input() category:any;
   
   YouMayLikeProducts:any[]=[];
->>>>>>> Stashed changes
   paginatedProducts: any[] = [];
   currentPage: number = 0;
   itemsPerPage: number = 4; // Default value
   totalPages: number = 0;
-<<<<<<< Updated upstream
-
-  constructor(
-    private cartservice: CartService,
-    private productservice: ProductService,
-    private router: Router
-  ) {}
-
-  ngOnInit() {
-    this.checkScreenSize();
-    window.addEventListener('resize', () => this.checkScreenSize());
-    
-    this.productservice.getProducts().subscribe((data: any) => {
-      this.YouMayLikeProducts = data;
-=======
   constructor(private cartservice:CartService,private productservice:ProductsService,private router:Router) {}
   ngOnInit(){
     this.productservice.getProductsByCategory(this.category).subscribe((data:any)=>{
       this.YouMayLikeProducts=data;
       console.log(this.YouMayLikeProducts);
       this.totalPages = Math.ceil(this.YouMayLikeProducts.length / this.itemsPerPage);
->>>>>>> Stashed changes
       this.updatePagination();
     });
   }
