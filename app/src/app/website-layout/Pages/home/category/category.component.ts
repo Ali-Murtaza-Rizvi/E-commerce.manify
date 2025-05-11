91912962e17e7bcf1dbd91c5850e231ea21a8285
Faris0302 <syedfaris.ali0302@gmail.com>
2025-05-12 02:32:38 +0500

    Category Pagination Applied

--- app/src/app/website-layout/Pages/home/category/category.component.ts
+++ app/src/app/website-layout/Pages/home/category/category.component.ts
@@ -1,41 +1,52 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-interface categories{
-  image:string;
-  title:string;
+import { Component, OnInit } from '@angular/core';
+
+interface Category {
+  image: string;
+  title: string;
 }
+
 @Component({
   selector: 'app-category',
   imports: [CommonModule],
   templateUrl: './category.component.html',
   styleUrl: './category.component.scss'
 })
-export class CategoryComponent {
-  Categories:categories[] = [
-    {
-      image:'assests/first.jpg',
-      title:'Shoes Collection'
-    },
-    {
-      image:'assests/fourth.jpg',
-      title:'Watches Collection'
-    },
-    {
-      image:'assests/seventh.jpg',
-      title:'Suits Collection'
-    },
-    {
-      image:'assests/sixth.jpg',
-      title:'Shirts Collection'
-    },
-    {
-      image:'assests/shorts.jpg',
-      title:'Active Wear Collection'
-    },
-    {
-      image:'assests/fifth.jpg',
-      title:'Pants Collection'
-    }
+export class CategoryComponent implements OnInit {
+  Categories: Category[] = [
+    { image: 'assests/first.jpg', title: 'Shoes Collection' },
+    { image: 'assests/fourth.jpg', title: 'Watches Collection' },
+    { image: 'assests/seventh.jpg', title: 'Suits Collection' },
+    { image: 'assests/sixth.jpg', title: 'Shirts Collection' },
+    { image: 'assests/shorts.jpg', title: 'Active Wear Collection' },
+    { image: 'assests/fifth.jpg', title: 'Pants Collection' }
+  ];
 
-  ]
+  paginatedCategories: Category[] = [];
+  currentPage: number = 1;
+  itemsPerPage: number = 3;
+
+  ngOnInit() {
+    this.updatePagination();
+  }
+
+  updatePagination() {
+    const start = (this.currentPage - 1) * this.itemsPerPage;
+    const end = start + this.itemsPerPage;
+    this.paginatedCategories = this.Categories.slice(start, end);
+  }
+
+  nextPage() {
+    if (this.currentPage * this.itemsPerPage < this.Categories.length) {
+      this.currentPage++;
+      this.updatePagination();
+    }
+  }
+
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updatePagination();
+    }
+  }
 }
